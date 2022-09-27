22dae9da9e22fd8b387e05958f8902b1c1d56e3d
giogege <giorgi.gegeshidze@exactprosystems.com>
2022-09-27 18:50:13 +0400

    added CTA buttons

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -16,14 +16,54 @@
 
 import React from 'react';
 import MUIButton, { ButtonProps as MUIButtonProps } from '@mui/material/Button';
+import { styled } from '@mui/material';
 
 export type ButtonProps = Pick<MUIButtonProps, 'children' | 'onClick' | 'sx' | 'disabled'> & {
-	onLabel?: string;
-	offLabel?: string;
+	buttonType: 'primary' | 'secondary';
 };
 
+const StyledButton = styled(
+	MUIButton,
+	{},
+)<ButtonProps>(({ buttonType }) => ({
+	borderRadius: '8px',
+	textTransform: 'none',
+	fontStyle: 'normal',
+	fontWeight: 500,
+	fontSize: '14px',
+	lineHeight: '16px',
+	padding: '8px 24px 8px 24px',
+	boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
+	boxSizing: 'border-box',
+	...(buttonType === 'primary' && {
+		border: '1px solid #4E8AFF',
+		backgroundColor: '#4E8AFF',
+		'&:hover': { backgroundColor: 'rgba(113, 162, 255, 1)' },
+		'&:focus': {
+			backgroundColor: 'rgba(221, 233, 255, 1)',
+			boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
+			color: 'rgba(78, 138, 255, 1)',
+		},
+	}),
+	...(buttonType === 'secondary' && {
+		color: '#4E8AFF',
+		borderColor: '#4E8AFF',
+
+		'&:hover': {
+			color: 'rgba(113, 162, 255, 1)',
+			borderColor: 'rgba(113, 162, 255, 1)',
+		},
+		'&:focus': {
+			color: 'rgba(88, 88, 88, 1)',
+			borderColor: 'rgba(88, 88, 88, 1)',
+		},
+	}),
+}));
+
 export function Button(props: ButtonProps) {
-	return <MUIButton variant='contained' {...props} />;
+	const variant = props.buttonType === 'primary' ? 'contained' : 'outlined';
+
+	return <StyledButton variant={variant} {...props} />;
 }
 
 Button.defaultProps = {
