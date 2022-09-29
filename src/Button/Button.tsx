c0947c7d3f14c30826217be6dc0c1fe15a9be05d
giogege <giorgi.gegeshidze@exactprosystems.com>
2022-09-29 19:30:44 +0400

    added small button and colors

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -17,15 +17,17 @@
 import React from 'react';
 import MUIButton, { ButtonProps as MUIButtonProps } from '@mui/material/Button';
 import { styled } from '@mui/material';
+import { Colors } from '../helpers';
 
 export type ButtonProps = Pick<MUIButtonProps, 'children' | 'onClick' | 'sx' | 'disabled'> & {
-	buttonType: 'primary' | 'secondary';
+	buttonType: 'primary' | 'secondary' | 'small';
 };
 
 const StyledButton = styled(
 	MUIButton,
 	{},
 )<ButtonProps>(({ buttonType }) => ({
+	border: '1px solid',
 	borderRadius: '8px',
 	textTransform: 'none',
 	fontStyle: 'normal',
@@ -33,37 +35,67 @@ const StyledButton = styled(
 	fontSize: '14px',
 	lineHeight: '16px',
 	padding: '8px 24px 8px 24px',
-	boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
-	boxSizing: 'border-box',
 	...(buttonType === 'primary' && {
-		border: '1px solid #4E8AFF',
-		backgroundColor: '#4E8AFF',
-		'&:hover': { backgroundColor: 'rgba(113, 162, 255, 1)' },
-		'&:focus': {
-			backgroundColor: 'rgba(221, 233, 255, 1)',
+		borderColor: Colors.primary[100],
+		backgroundColor: Colors.primary[100],
+		boxShadow: ' 0px 0.5px 2px rgba(0, 0, 0, 0.1)',
+		'&:hover': {
+			borderColor: Colors.primary[80],
+			backgroundColor: Colors.primary[80],
 			boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
-			color: 'rgba(78, 138, 255, 1)',
+		},
+		'&:focus': {
+			backgroundColor: Colors.primary[24],
+			borderColor: Colors.primary[24],
+			boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
+			color: Colors.primary[100],
 		},
 	}),
 	...(buttonType === 'secondary' && {
-		color: '#4E8AFF',
-		borderColor: '#4E8AFF',
-
+		color: Colors.primary[100],
+		borderColor: Colors.primary[100],
+		backgroundColor: 'transparent',
+		boxShadow: 'none',
+		filter: 'drop-shadow(0px 0.5px 2px rgba(0, 0, 0, 0.1))',
 		'&:hover': {
-			color: 'rgba(113, 162, 255, 1)',
-			borderColor: 'rgba(113, 162, 255, 1)',
+			boxShadow: 'none',
+			color: Colors.primary[80],
+			borderColor: Colors.primary[80],
+			backgroundColor: 'transparent',
+			filter: 'drop-shadow(0px 2px 3px rgba(0, 0, 0, 0.15))',
 		},
 		'&:focus': {
-			color: 'rgba(88, 88, 88, 1)',
-			borderColor: 'rgba(88, 88, 88, 1)',
+			boxShadow: 'none',
+			color: Colors.grey[80],
+			borderColor: Colors.grey[80],
+			backgroundColor: 'transparent',
+			filter: 'drop-shadow(0px 0.5px 2px rgba(0, 0, 0, 0.1))',
+		},
+	}),
+	...(buttonType === 'small' && {
+		background: Colors.white[100],
+		boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
+		borderRadius: '16px',
+		color: Colors.primary[100],
+		border: '1px solid',
+		borderColor: '#FFFFFF',
+		'&:hover': {
+			borderColor: Colors.lightGrey[60],
+			backgroundColor: Colors.lightGrey[60],
+			boxShadow: '0px 2px 3px rgba(0, 0, 0, 0.15)',
+			color: Colors.primary[80],
+		},
+		'&:focus': {
+			backgroundColor: Colors.primary[24],
+			borderColor: Colors.primary[24],
+			boxShadow: '0px 0.5px 2px rgba(0, 0, 0, 0.1)',
+			color: Colors.primary[100],
 		},
 	}),
 }));
 
 export function Button(props: ButtonProps) {
-	const variant = props.buttonType === 'primary' ? 'contained' : 'outlined';
-
-	return <StyledButton variant={variant} {...props} />;
+	return <StyledButton variant={'contained'} {...props} />;
 }
 
 Button.defaultProps = {
---
+++ src/helpers/Colors.ts
@@ -0,0 +1,37 @@
+export const Colors = {
+	primary: {
+		[100]: '#4E8AFF',
+		[80]: '#71A2FF',
+		[60]: '#AAC7FF',
+		[40]: '#C6DAFF',
+		[24]: '#DDE9FF',
+		[16]: '#E3ECFF',
+	},
+	grey: {
+		[100]: '#3A3A3A',
+		[80]: '#585858',
+		[60]: '#898989',
+		[40]: '#B0B0B0',
+		[24]: '#D0D0D0',
+		[16]: '#E0E0E0',
+	},
+	lightGrey: {
+		[100]: '#F1F1F1',
+		[80]: '#F8F8F8',
+		[60]: '#FBFBFB',
+	},
+	system: {
+		messages: '#19C3E5',
+		events: '#F26C0D',
+		bookmarks: '#A65CD6',
+		search: '#1D19E5',
+	},
+	status: {
+		passed: '#52FF4E',
+		error: '#FF4E4E',
+		warning: '#FFB84E',
+	},
+	white: {
+		[100]: '#FFFFFF',
+	},
+};
